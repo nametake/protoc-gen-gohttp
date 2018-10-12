58b6ee374d62e56f469f5e255e41df22b7718e85
nametake <nametake.kyarabuki@gmail.com>
2018-10-12 16:05:08 +0900

    Implemented P method

--- generator/generator.go
+++ generator/generator.go
@@ -1,8 +1,17 @@
 package generator
 
+import "io"
+
 type Generator struct {
+	w io.Writer
 }
 
 func New() *Generator {
 	return &Generator{}
+}
+
+func (g *Generator) P(args ...string) {
+	for _, v := range args {
+		io.WriteString(g.w, v)
+	}
 }
