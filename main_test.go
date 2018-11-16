efa2fe1d8280d6e398a0295711c920136de2667f
nametake <nametake.kyarabuki@gmail.com>
2018-11-16 14:26:39 +0900

    Find all proto file in testdata

--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -12,4 +14,20 @@ func TestGolden(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(workdir)
+
+	// find all proto file in testdata.
+	packages := map[string][]string{}
+	if err := filepath.Walk("testdata", func(path string, info os.FileInfo, err error) error {
+		if !strings.HasSuffix(path, ".proto") {
+			return nil
+		}
+
+		dir := filepath.Dir(path)
+		packages[dir] = append(packages[dir], path)
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(packages)
 }
