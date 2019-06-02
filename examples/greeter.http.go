ccca144ea3f5a04df40dcf80ca8553deee9afa82
nametake <nametake.kyarabuki@gmail.com>
2019-06-03 01:00:57 +0900

    Update example

--- examples/greeter.http.go
+++ examples/greeter.http.go
@@ -26,17 +26,95 @@ func NewGreeterHTTPConverter(srv GreeterServer) *GreeterHTTPConverter {
 }
 
 func (h *GreeterHTTPConverter) SayHello(cb func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error)) http.HandlerFunc {
-}
-
-type GreeterHTTPConverter struct {
-	srv GreeterServer
-}
-
-func NewGreeterHTTPConverter(srv GreeterServer) *GreeterHTTPConverter {
-	return &GreeterHTTPConverter{
-		srv: srv,
+	if cb == nil {
+		cb = func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error) {
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				p := status.New(codes.Unknown, err.Error()).Proto()
+				switch r.Header.Get("Content-Type") {
+				case "application/protobuf", "application/x-protobuf":
+					buf, err := proto.Marshal(p)
+					if err != nil {
+						return
+					}
+					if _, err := io.Copy(w, bytes.NewBuffer(buf)); err != nil {
+						return
+					}
+				case "application/json":
+					if err := json.NewEncoder(w).Encode(p); err != nil {
+						return
+					}
+				default:
+				}
+			}
+		}
 	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			cb(ctx, w, r, nil, nil, err)
+			return
+		}
+
+		arg := &HelloRequest{}
+
+		contentType := r.Header.Get("Content-Type")
+		switch contentType {
+		case "application/protobuf", "application/x-protobuf":
+			if err := proto.Unmarshal(body, arg); err != nil {
+				cb(ctx, w, r, nil, nil, err)
+				return
+			}
+		case "application/json":
+			if err := jsonpb.Unmarshal(bytes.NewBuffer(body), arg); err != nil {
+				cb(ctx, w, r, nil, nil, err)
+				return
+			}
+		default:
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			_, err := fmt.Fprintf(w, "Unsupported Content-Type: %s", contentType)
+			cb(ctx, w, r, nil, nil, err)
+			return
+		}
+
+		ret, err := h.srv.SayHello(ctx, arg)
+		if err != nil {
+			cb(ctx, w, r, arg, nil, err)
+			return
+		}
+
+		switch contentType {
+		case "application/protobuf", "application/x-protobuf":
+			buf, err := proto.Marshal(ret)
+			if err != nil {
+				cb(ctx, w, r, arg, ret, err)
+				return
+			}
+			if _, err := io.Copy(w, bytes.NewBuffer(buf)); err != nil {
+				cb(ctx, w, r, arg, ret, err)
+				return
+			}
+		case "application/json":
+			m := jsonpb.Marshaler{
+				EnumsAsInts:  true,
+				EmitDefaults: true,
+			}
+			if err := m.Marshal(w, ret); err != nil {
+				cb(ctx, w, r, arg, ret, err)
+				return
+			}
+		default:
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			_, err := fmt.Fprintf(w, "Unsupported Content-Type: %s", contentType)
+			cb(ctx, w, r, arg, ret, err)
+			return
+		}
+		cb(ctx, w, r, arg, ret, nil)
+	})
 }
 
-func (h *GreeterHTTPConverter) SayHello(cb func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error)) http.HandlerFunc {
+func (h *GreeterHTTPConverter) SayHelloWithName(cb func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error)) (string, string, http.HandlerFunc) {
+	return "Greeter", "SayHello", h.SayHello(cb)
 }
--- generator.go
+++ generator.go
@@ -97,7 +97,7 @@ func (g *Generator) genTarget(file *descriptor.FileDescriptorProto) *targetFile 
 		for _, m := range s.GetMethod() {
 			method := &targetMethod{
 				Name: m.GetName(),
-				Arg:  m.GetInputType(),
+				Arg:  ioname(m.GetInputType()),
 			}
 			if !m.GetServerStreaming() && !m.GetClientStreaming() {
 				service.Methods = append(service.Methods, method)
@@ -106,7 +106,6 @@ func (g *Generator) genTarget(file *descriptor.FileDescriptorProto) *targetFile 
 		if len(service.Methods) > 0 {
 			tFile.Services = append(tFile.Services, service)
 		}
-		tFile.Services = append(tFile.Services, service)
 	}
 
 	if len(tFile.Services) <= 0 {
@@ -130,142 +129,6 @@ func (g *Generator) genRespFile(target *targetFile) *plugin.CodeGeneratorRespons
 		Name:    proto.String(basename(target.Name) + ".http.go"),
 		Content: proto.String(buf.String()),
 	}
-}
-
-func (g *Generator) writePackage(w io.Writer, f *descriptor.FileDescriptorProto) {
-	p(w, "package %s", f.Options.GetGoPackage())
-	p(w, "")
-}
-
-func (g *Generator) writeImports(w io.Writer) {
-	p(w, "import (")
-	p(w, "	\"bytes\"")
-	p(w, "	\"context\"")
-	p(w, "	\"encoding/json\"")
-	p(w, "	\"fmt\"")
-	p(w, "	\"io\"")
-	p(w, "	\"io/ioutil\"")
-	p(w, "	\"net/http\"")
-	p(w, "")
-	p(w, "	\"github.com/golang/protobuf/jsonpb\"")
-	p(w, "	\"github.com/golang/protobuf/proto\"")
-	p(w, "	\"google.golang.org/grpc/codes\"")
-	p(w, "	\"google.golang.org/grpc/status\"")
-	p(w, ")")
-	p(w, "")
-}
-
-func (g *Generator) writeService(w io.Writer, s *descriptor.ServiceDescriptorProto) {
-	p(w, "type %sHTTPConverter struct{", s.GetName())
-	p(w, "	srv %sServer", s.GetName())
-	p(w, "}")
-	p(w, "")
-	p(w, "func New%sHTTPConverter(srv %sServer) *%sHTTPConverter {", s.GetName(), s.GetName(), s.GetName())
-	p(w, "	return &%sHTTPConverter{", s.GetName())
-	p(w, "		srv: srv,")
-	p(w, "	}")
-	p(w, "}")
-	p(w, "")
-}
-
-func (g *Generator) writeMethod(w io.Writer, s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
-	p(w, "func (h *%sHTTPConverter) %s(cb func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error)) http.HandlerFunc {", s.GetName(), m.GetName())
-	p(w, "	if cb == nil {")
-	p(w, "		cb = func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error) {")
-	p(w, "			if err != nil {")
-	p(w, "				w.WriteHeader(http.StatusInternalServerError)")
-	p(w, "				p := status.New(codes.Unknown, err.Error()).Proto()")
-	p(w, "				switch r.Header.Get(\"Content-Type\") {")
-	p(w, "				case \"application/protobuf\", \"application/x-protobuf\":")
-	p(w, "					buf, err := proto.Marshal(p)")
-	p(w, "					if err != nil {")
-	p(w, "						return")
-	p(w, "					}")
-	p(w, "					if _, err := io.Copy(w, bytes.NewBuffer(buf)); err != nil {")
-	p(w, "						return")
-	p(w, "					}")
-	p(w, "				case \"application/json\":")
-	p(w, "					if err := json.NewEncoder(w).Encode(p); err != nil {")
-	p(w, "						return")
-	p(w, "					}")
-	p(w, "				default:")
-	p(w, "				}")
-	p(w, "			}")
-	p(w, "		}")
-	p(w, "	}")
-	p(w, "	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {")
-	p(w, "		ctx := r.Context()")
-	p(w, "")
-	p(w, "		body, err := ioutil.ReadAll(r.Body)")
-	p(w, "		if err != nil {")
-	p(w, "			cb(ctx, w, r, nil, nil, err)")
-	p(w, "			return")
-	p(w, "		}")
-	p(w, "")
-	p(w, "		arg := &%s{}", ioname(m.GetInputType()))
-	p(w, "")
-	p(w, "		contentType := r.Header.Get(\"Content-Type\")")
-	p(w, "		switch contentType {")
-	p(w, "		case \"application/protobuf\", \"application/x-protobuf\":")
-	p(w, "			if err := proto.Unmarshal(body, arg); err != nil {")
-	p(w, "				cb(ctx, w, r, nil, nil, err)")
-	p(w, "				return")
-	p(w, "			}")
-	p(w, "		case \"application/json\":")
-	p(w, "			if err := jsonpb.Unmarshal(bytes.NewBuffer(body), arg); err != nil {")
-	p(w, "				cb(ctx, w, r, nil, nil, err)")
-	p(w, "				return")
-	p(w, "			}")
-	p(w, "		default:")
-	p(w, "			w.WriteHeader(http.StatusUnsupportedMediaType)")
-	p(w, "			_, err := fmt.Fprintf(w, \"Unsupported Content-Type: %%s\", contentType)")
-	p(w, "			cb(ctx, w, r, nil, nil, err)")
-	p(w, "			return")
-	p(w, "		}")
-	p(w, "")
-	p(w, "		ret, err := h.srv.%s(ctx, arg)", m.GetName())
-	p(w, "		if err != nil {")
-	p(w, "			cb(ctx, w, r, arg, nil, err)")
-	p(w, "			return")
-	p(w, "		}")
-	p(w, "")
-	p(w, "		switch contentType {")
-	p(w, "		case \"application/protobuf\", \"application/x-protobuf\":")
-	p(w, "			buf, err := proto.Marshal(ret)")
-	p(w, "			if err != nil {")
-	p(w, "				cb(ctx, w, r, arg, ret, err)")
-	p(w, "				return")
-	p(w, "			}")
-	p(w, "			if _, err := io.Copy(w, bytes.NewBuffer(buf)); err != nil {")
-	p(w, "				cb(ctx, w, r, arg, ret, err)")
-	p(w, "				return")
-	p(w, "			}")
-	p(w, "		case \"application/json\":")
-	p(w, "			m := jsonpb.Marshaler{")
-	p(w, "				EnumsAsInts:  true,")
-	p(w, "				EmitDefaults: true,")
-	p(w, "			}")
-	p(w, "			if err := m.Marshal(w, ret); err != nil {")
-	p(w, "				cb(ctx, w, r, arg, ret, err)")
-	p(w, "				return")
-	p(w, "			}")
-	p(w, "		default:")
-	p(w, "			w.WriteHeader(http.StatusUnsupportedMediaType)")
-	p(w, "			_, err := fmt.Fprintf(w, \"Unsupported Content-Type: %%s\", contentType)")
-	p(w, "			cb(ctx, w, r, arg, ret, err)")
-	p(w, "			return")
-	p(w, "		}")
-	p(w, "		cb(ctx, w, r, arg, ret, nil)")
-	p(w, "	})")
-	p(w, "}")
-	p(w, "")
-}
-
-func (g *Generator) writeMethodWithPath(w io.Writer, s *descriptor.ServiceDescriptorProto, m *descriptor.MethodDescriptorProto) {
-	p(w, "func (h *%sHTTPConverter) %sWithName(cb func(ctx context.Context, w http.ResponseWriter, r *http.Request, arg, ret proto.Message, err error)) (string, string, http.HandlerFunc) {", s.GetName(), m.GetName())
-	p(w, "	return \"%s\", \"%s\", h.%s(cb)", s.GetName(), m.GetName(), m.GetName())
-	p(w, "}")
-	p(w, "")
 }
 
 func basename(name string) string {
