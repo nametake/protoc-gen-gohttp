350e7fd4c3f47570c60bcf75085708a34a6a6cf8
nametake <nametake.kyarabuki@gmail.com>
2020-09-01 22:53:47 +0900

    Added GenerateFile

---
+++ gengohttp.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func GenerateFile(gen *protogen.Plugin, file *protogen.File) *protogen.GeneratedFile {
+	panic("not implemented")
+}
